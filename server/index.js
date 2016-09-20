4242943d55be2f5e39bf57936812b83885824291
Jen Yee <jenyee1022@yahoo.com>
2016-09-20 01:14:49 +0000

    updated to be able to post tweet and styled up nav and new tweet box

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+//sudo mongod --config /etc/mongod.conf
 "use strict";
 
 require('dotenv').config();
@@ -9,9 +10,23 @@ const app         = express();
 
 const tweetsApi  = require('./api/tweets');
 const db         = require('./lib/db');
+const collectionName = "tweets";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+db.connect((dbInstance) => {
+  app.post("/", (req, res) => {
+    db.collection(collectionName).insert({"content":{"text": req.body.text}}, function(err){
+      if (err) {
+        return res.render(err);
+      }
+      res.redirect("/tweets/")
+    });
+  });
+
+});
+
 
 db.connect((dbInstance) => {
   app.use('/tweets', tweetsApi(dbInstance));
